7cc1198d7d25d22c9da301b13bee2ecaa381cbfa
Alecia Benjamin <aleciabenjamin@gmail.com>
2019-08-29 10:27:56 +0200

    clean up code

--- src/components/App.js
+++ src/components/App.js
@@ -1,87 +1,82 @@
-import React from 'react';
-import Header from './Header';
-import Player from './Player';
-import AddPlayerForm from './AddPlayerForm';
+import React from "react";
+import AddPlayerForm from "./AddPlayerForm";
+import Header from "./Header";
+import Player from "./Player";
 
 class App extends React.Component {
   state = {
     players: [
       {
-				name: "Guil",
-				score: 0,
+        name: "Dennis",
+        score: 0,
         id: 1
       },
       {
-				name: "Treasure",
-				score: 0,
+        name: "Dee",
+        score: 0,
         id: 2
       },
       {
-				name: "Ashley",
-				score: 0,
+        name: "Charlie",
+        score: 0,
         id: 3
       },
       {
-				name: "James",
-				score: 0,
+        name: "Mac",
+        score: 0,
         id: 4
       }
     ]
-	};
+  };
 
-	// player id counter
-	prevPlayerId = 4;
+  prevPlayerId = 4;
 
-	handleScoreChange = (index, delta) => {
-    this.setState( prevState => ({
-      score: prevState.players[index].score += delta
-		}));
-	}
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta)
+    }));
+  };
 
-	handleAddPlayer = (name) => {
-		this.setState( prevState => {
-			return {
-			players: [
-				...prevState.players,
-				{
-					name,
-					score: 0,
-					id: this.prevPlayerId += 1
-				}
-			]
-		}
-		})
-	}
+  handleAddPlayer = name => {
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1)
+          }
+        ]
+      };
+    });
+  };
 
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
       };
     });
-  }
+  };
 
   render() {
     return (
       <div className="scoreboard">
-        <Header
-          title="Scoreboard"
-          players={this.state.players}
-        />
+        <Header title="Scoreboard" players={this.state.players} />
 
-        {/* Players list */}
-        {this.state.players.map( (player, index) =>
+        {this.state.players.map((player, index) => (
           <Player
-						name={player.name}
-						score={player.score}
+            name={player.name}
+            score={player.score}
             id={player.id}
-						key={player.id.toString()}
-						index={index}
-						changeScore={this.handleScoreChange}
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
-				)}
-				<AddPlayerForm addPlayer={this.handleAddPlayer}/>
+        ))}
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
       </div>
     );
   }
